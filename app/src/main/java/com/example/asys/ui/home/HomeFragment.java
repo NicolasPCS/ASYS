0a6cfb4925152e133c86e22c04f0b23fc96b5867
Nicolás <nicolascaytuirosilva@gmail.com>
2022-05-03 10:44:45 -0500

    Material design implementation

--- app/src/main/java/com/example/asys/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/asys/ui/home/HomeFragment.java
@@ -1,28 +1,59 @@
 package com.example.asys.ui.home;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.asys.R;
 import com.example.asys.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    LinearLayout expandableView;
+    Button expandBtn;
+    CardView cardView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        expandableView = (LinearLayout) root.findViewById(R.id.expandableLayout);
+        expandBtn = (Button) root.findViewById(R.id.expandBtn);
+        cardView = (CardView) root.findViewById(R.id.card_course_item);
+
+        expandBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expandableView.getVisibility() == View.GONE) {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.VISIBLE);
+                    expandBtn.setText("OCULTAR");
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.GONE);
+                    expandBtn.setText("MARCAR ASISTENCIA");
+                }
+            }
+        });
+
 
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
