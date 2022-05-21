61401d446e01c5025c44e2d1ee693f2ce94a8940
Nicolás <nicolascaytuirosilva@gmail.com>
2022-05-21 17:57:38 -0500

    ChatBot feature added - Version 3.0

--- app/src/main/java/com/example/asys/Interfaces/BotReply.java
+++
@@ -1,7 +0,0 @@
-package com.example.asys.Interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-    void callback(DetectIntentResponse returnResponse);
-}
